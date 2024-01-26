7a897d3c1fd4487286c14721a9124ca2e1ea05f6
Lia <liazerqueraf@gmail.com>
2024-01-26 02:19:35 -0500

    tests done

--- exercises/17_tests/tests3.rs
+++ exercises/17_tests/tests3.rs
@@ -7,7 +7,7 @@
 // Execute `rustlings hint tests3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 pub fn is_even(num: i32) -> bool {
     num % 2 == 0
@@ -19,11 +19,14 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        let a = 2;
+
+        assert!(is_even(2));
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!();
+        let a = 3;
+        assert!(!is_even(3));
     }
 }
